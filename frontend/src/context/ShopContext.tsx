5e351b32bbe595da4f509b8db50390c9425c7dd5
Zzamanapu <mdsaiduzzamanapu@gmail.com>
2026-02-05 21:38:50 +0600

    Complete: Colleciton page

--- frontend/src/context/ShopContext.tsx
+++ frontend/src/context/ShopContext.tsx
@@ -1,34 +1,53 @@
-import { createContext, ReactNode } from "react";
-import { products, type Product } from "../assets/assets";
+import { createContext, useState, type ReactNode } from "react";
+import type { Product } from "../types/assets";
+import { products } from "../assets/assets";
+// import { products, type Product } from "../assets/assets";
 
 interface ShopContextType {
   products: Product[];
   currency: string;
   delivery_fee: number;
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  showSearch: boolean;
+  setShowSearch: React.Dispatch<React.SetStateAction<boolean>>;
 }
 export const ShopContext = createContext<ShopContextType>({
   products: [],
   currency: '$',
   delivery_fee: 10,
+  search: '',
+  setSearch: () => { },
+  showSearch: false,
+  setShowSearch: () => { },
 });
 
 
 interface ShopContextProviderProps {
   children: ReactNode;
-}
+};
+
 
 
 const ShopContextProvider = ({ children }: ShopContextProviderProps) => {
+  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
   const value: ShopContextType = {
-    products, currency: '$', delivery_fee: 10,
+    products,
+    currency: '$',
+    delivery_fee: 10,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   }
 
   return (
     <ShopContext.Provider value={value}>
       {children}
     </ShopContext.Provider>
-  )
-}
+  );
+};
 
 
 export default ShopContextProvider;
